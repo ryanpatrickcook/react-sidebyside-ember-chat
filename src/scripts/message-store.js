1a7fc44287a1dc5f2839d1966c0ca0e567ed242d
Ryan Cook <ryanpatrickcook@gmail.com>
2016-08-24 23:25:00 -0400

    add message service

---
+++ src/scripts/message-store.js
@@ -0,0 +1,38 @@
+import EventEmitter from 'events';
+
+const emitter = new EventEmitter();
+var messages = [];
+
+export default {
+  getMessages() {
+    return messages.concat();
+  },
+
+  subscribe(callback) {
+    emitter.on('update', callback);
+  },
+
+  unsubscribe(callback) {
+    emitter.off('update', callback);
+  },
+
+  newMessage(message) {
+    let msg = {
+      id: Date.now() + Math.round(Math.random()*10000),
+      text: message
+    };
+
+    messages.push(msg);
+    emitter.emit('update');
+  },
+
+  updateMessage(message, text) {
+    messages.forEach((m) => {
+      if(m.id == message.id) {
+        m.text = text;
+        return;
+      }
+    });
+    emitter.emit('update');
+  }
+};
